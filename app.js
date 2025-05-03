4d6a32be2c35226941f89903d983affb381704bf
Onakice <pongsapak.tatongjai+1@gmail.com>
2025-05-03 23:21:45 +0700

    Use template

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 // set DEBUG=* & node app.js
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
 
-    res.send("Hello borntoDev Co., Ltd. testtest");
+    res.send("Hello borntoDev Co., Ltd.");
 
 })
 
