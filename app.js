289457b8f340c46068a2b5c53ea743095d26d193
Onakice <pongsapak.tatongjai+1@gmail.com>
2025-05-03 20:53:26 +0700

    Add all files of Project
    Hello World :100:

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+// set DEBUG=* & node app.js
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req, res) => {
+
+    res.send("Hello borntoDev Co., Ltd.");
+
+})
+
+app.listen(port, () => {
+    debug("Listening on port" + chalk.green(" : " +port));
+})
